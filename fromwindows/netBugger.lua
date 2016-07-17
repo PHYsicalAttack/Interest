c8da670ce0c5639d1c047ba96a98a621e68ca369
Fallever <piscovip@163.com>
2016-07-17 20:17:59 +0800

    update

--- fromwindows/netBugger.lua
+++ fromwindows/netBugger.lua
@@ -7,11 +7,12 @@ function dealurl(url,keyword,startpage,endpage)
  	--如果只传入3个参数则，设置startpage为20，endpage为传入的值
  	if endpage == nil then
  		endpage = startpage
- 		startpage=20
+ 		startpage=0
  	end
 	local downt={}
 	for i = startpage,endpage,20 do
-		pageurl = string.format(url,keyword,startpage,endpage)
+		pageurl = string.format(url,keyword,i)
+		print("Deal with pageurl: ".. pageurl)
 		local res,err= http.request(pageurl)
 		if  err == 200 then
 			for l in string.gmatch(res,"objURL\34:\34http://.-%.jpg") do
@@ -35,23 +36,22 @@ function dealurl(url,keyword,startpage,endpage)
 end
 --下载图片
 local keyword="轻音少女"
-local startpage =20
-local endpage =300
+local startpage =0
+local endpage =500
 local dir = "F:/netbuger/"
-os.execute("mkdir ".. string.gsub(dir,"/","\\").. keyword)
+res,err= os.execute("mkdir ".. string.gsub(dir,"/","\\").. keyword)
+print(res , err)
 for i,v in ipairs(dealurl(url,keyword,endpage)) do
-	local pretime=os.time()
+	local pretime=socket.gettime()
 	local ext = string.match(v,"%..+",-5)
 	local file = io.open(dir .. keyword .. "/".. i ..ext,"wb")
 	local res,err = http.request(v)
-	local endtime =os.time()
+	local endtime =socket.gettime()
 	if err == 200 then
 		file:write(res)
 		file:close()
-		print("pay " .. endtime-pretime .. "s,download success!!")
+		print("used " .. string.format("%.2f",endtime-pretime) .. "s,download " .. i ..  " pic success!!")
 	else
-		print("pay" .. endtime-pretime .. "s,download from " .. v .." failed because of " .. err)
+		print("used " .. string.format("%.2f",endtime-pretime) .. "s,download " .. i ..  " pic from " .. v .." failed because of " .. err)
 	end
 end
-
-
